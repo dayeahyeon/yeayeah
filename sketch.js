c07aba23e9dde7a53c707be3927e724f9fb3bcfb
dayeahyeon <101611188+dayeahyeon@users.noreply.github.com>
2022-04-11 00:05:48 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,105 +1,148 @@
-let d = 100;
-let th = 1;
+let rad = 100;
+let x1pos, y1pos;
+let x2pos, y2pos;
+let x3pos, y3pos;
+let x4pos, y4pos;
+let x5pos, y5pos;
 
-let d1 = 100;
-let th1 = 1;
+let x1speed = 5
+let y1speed = 5
 
-let posX = new Array(100);
-let posY = new Array(100);
-let posVX = new Array(100);
-let posVY = new Array(100);
+let x2speed = 5
+let y2speed = 5
+
+let x3speed = 4
+let y3speed = 4
+
+let x4speed = 3
+let y4speed = 3
+
+let x5speed = 4
+let y5speed = 4
 
 
-function setup() {
-  createCanvas(400, 400);
-  angleMode(DEGREES)
-  background(60);
-  ellipseMode(CENTER)
-  
-   for (let j=0; j<10; j++) {
-    posX[j] = width*2;
-    posY[j] = height*2;
-    posVX[j] = 0;
-    posVY[j] = 0;
-   }
-}
+let x1direction = 1;
+let y1direction = 1;
+let x2direction = 1;
+let y2direction = -1;
+let x3direction = 2;
+let y3direction = -3;
+let x4direction = 5;
+let y4direction = -1;
+let x5direction = 2;
+let y5direction = -2;
 
-function draw() {
-  
-  for (i=0; i<50; i++)
-  stroke(255-i, i)
-  translate(width/2, height/2);
-  fill(255-i, i)
-  
-  
-  th = th + 0.2
-  let x1 = d * cos(th);
-  let y1 = d * sin(th);
-  
-  line(x1, y1, 0, 0)
+
+
+function setup(){
+  createCanvas(800,800);
+  noStroke();
+  frameRate(30);
+  ellipseMode(RADIUS);
+  x1pos = width/2;
+  y1pos = height/2;
+  x2pos = width/3;
+  y2pos = height/3;
+  x3pos = width/4;
+  y3pos = height/4;
+  x4pos = width/6;
+  y4pos = height/6;
+  x5pos = width/2;
+  y5pos = height/2;
  
-  
-  translate(width/5, height/5);
-  
-  
-  th = th + 0.8
-  let x2 = d * cos(th)/1.5;
-  let y2 = d * sin(th)/1.5;
-  
-  line( x2, y2, 0, 0)
-  
-  
-  translate(width/6, height/6);
-  
-  
-  th = th + 2
-  let x3 = d * cos(th)/2;
-  let y3 = d * sin(th)/2;
-  
-  line( x3, y3, 0, 0)
-  
-  
-  translate(width/-1.7, height/-1.7);
-  
-  
-  th = th + 1
-  let x4 = d * cos(th)*1.2;
-  let y4 = d * sin(th)*1.2;
-  
-  line(x4, y4, 0, 0)
-  
- th1 = th1 + 0.3
-  let x5 = d1 * cos(th1);
-  let y5 = d1 * sin(th1);
-  
-   for (let j=4; j>=0; j--) {
-    posX[j] = posX[j-1];
-    posY[j] = posY[j-1];
-    
-    
-  }
-    for (let j=100; j>=0; j--) {
-    posVX[j] = posVX[j-1];
-    posVY[j] = posVY[j-1];
-    }
-  
-  
-  posX[0] = d*cos(th)*3
-  posY[0] = th1 + 2.3
-  
-  posVX[0] = mouseX/2
-  posVY[0] = mouseY/2
-  
-  
-    for (let j=10; j>=0; j--) {
- stroke(0, 30)
-   noFill();
-    ellipse(posX[j], posY[j], j, j+j*60);
-      ellipse(posVX[j],posVY[j], j+j*5, j+j*5);
-    
-    
-  }
-  
-  
 }
 
+function draw(){
+  background(220);
+  
+  let from = color(255,150,0);
+  let to = color(0, 150, 255);
+  let pct = map(x1pos, rad, width-rad, 0, 1);
+  let c = lerpColor(from, to, pct);
+  fill(c)
+  ellipse (x1pos, y1pos, rad, rad);
+  
+  let from2 = color(205,150,0);
+  let to2 = color(0, 100, 255);
+  let pct2= map(x2pos, rad, width-rad, 0, 1);
+  let d = lerpColor(from2, to2, pct2);
+  fill(d)
+  ellipse (x2pos, y2pos, rad, rad);
+  
+  let from3 = color(25,150,0);
+  let to3 = color(0, 10, 205);
+  let pct3= map(x3pos, rad, width-rad, 0, 1);
+  let e = lerpColor(from3, to3, pct3);
+  fill(e)
+  ellipse (x3pos, y3pos, rad, rad);
+  
+  
+  let from4 = color(0,20,50);
+  let to4 = color(0, 60, 15);
+  let pct4= map(x4pos, rad, width-rad, 0, 1);
+  let f = lerpColor(from4, to4, pct4);
+  fill(f)
+  ellipse (x4pos, y4pos, rad, rad);
+  
+  let from5 = color(95,110,0);
+  let to5 = color(0, 10, 140);
+  let pct5= map(x5pos, rad, width-rad, 0, 1);
+  let g = lerpColor(from5, to5, pct5);
+  fill(g)
+  ellipse (x5pos, y5pos, rad, rad);
+  
+  
+  
+  
+  x1pos = x1pos + x1speed * x1direction ;
+  y1pos = y1pos + y1speed * y1direction ;
+  x2pos = x2pos + x2speed * x2direction ;
+  y2pos = y2pos + y2speed * y2direction ;
+  x3pos = x3pos + x3speed * x3direction ;
+  y3pos = y3pos + y3speed * y3direction ;
+  x4pos = x4pos + x4speed * x4direction ;
+  y4pos = y4pos + y4speed * y4direction ;
+  x5pos = x5pos + x5speed * x5direction ;
+  y5pos = y5pos + y5speed * y5direction ;
+  
+  
+  
+  
+  
+  
+  if (x1pos > width - rad || x1pos < rad){
+    x1direction *= -1 ;
+  }
+  if (y1pos > height - rad || y1pos < rad){
+    y1direction *= -1;
+  }
+  
+  
+  if (x2pos > width - rad || x2pos < rad){
+    x2direction *= -1 ;
+  }
+  if (y2pos > height - rad || y2pos < rad){
+    y2direction *= -1;
+  }
+  
+   if (x3pos > width - rad || x3pos < rad){
+    x3direction *= -1 ;
+  }
+  if (y3pos > height - rad || y3pos < rad){
+    y3direction *= -1;
+  }
+  
+   if (x4pos > width - rad || x4pos < rad){
+    x4direction *= -1 ;
+  }
+  if (y4pos > height - rad || y4pos < rad){
+    y4direction *= -1;
+  }
+  
+   if (x5pos > width - rad || x5pos < rad){
+    x5direction *= -1 ;
+  }
+  if (y5pos > height - rad || y5pos < rad){
+    y5direction *= -1;
+  }
+}
